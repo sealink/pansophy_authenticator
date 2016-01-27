b6da2483e743ab73ad197518736129c2c6b1e699
Alessandro Berardi <berardialessandro@gmail.com>
2016-01-27 14:52:04 +1030

    Removes test spec

--- spec/pansophy_authenticator_spec.rb
+++ spec/pansophy_authenticator_spec.rb
@@ -4,8 +4,4 @@ describe PansophyAuthenticator do
   it 'has a version number' do
     expect(PansophyAuthenticator::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
