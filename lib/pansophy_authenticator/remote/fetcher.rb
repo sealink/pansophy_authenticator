7e9d6238a05bd5fe1470bdca1dfcf178df289c1f
Alessandro Berardi <berardialessandro@gmail.com>
2016-04-07 13:33:46 +0930

    Handles invalid format of remote app keys file

--- lib/pansophy_authenticator/remote/fetcher.rb
+++ lib/pansophy_authenticator/remote/fetcher.rb
@@ -3,18 +3,37 @@ require 'pansophy'
 module PansophyAuthenticator
   module Remote
     class Fetcher
+      include Memoizable
+
       def keys
-        @keys ||= fetch
+        fetch
       end
+      memoize :keys
 
       private
 
       def fetch
-        YAML.load Pansophy.read(configuration.bucket_name, configuration.file_path)
+        fail_for_invalid_format! unless parsed_content.is_a? Hash
+        parsed_content
+      end
+
+      def parsed_content
+        YAML.load remote_content
+      rescue Psych::SyntaxError
+        fail_for_invalid_format!
+      end
+      memoize :parsed_content
+
+      def remote_content
+        Pansophy.read(configuration.bucket_name, configuration.file_path)
       end
 
       def configuration
         PansophyAuthenticator.configuration
+      end
+
+      def fail_for_invalid_format!
+        fail Error, 'Remote application keys file is not in a valid format'
       end
     end
   end
